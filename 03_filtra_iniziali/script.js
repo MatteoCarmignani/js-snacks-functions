f15797373f12db12a758197ff7aa2d142bffc5bb
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-03 13:50:31 +0200

    Finalizzazione

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function filtraParolePerInizio(names, lettera) {
+  const paroleConIniziale = [];
+
+
+  for (let i = 0; i < names.length; i++) {
+    const parolaCorrente = names[i];
+
+
+    if (parolaCorrente.startsWith(lettera)) {
+
+      paroleConIniziale.push(parolaCorrente);
+
+    }
+  }
+
+  return paroleConIniziale;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(filtraParolePerInizio(names, "A"))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
